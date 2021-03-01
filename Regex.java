40e86131b8b5065e84d8d218e7919f6afbdff4f7
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-01 22:26:12 +0530

    Added logic for validating first name

--- Regex.java
+++ Regex.java
@@ -1,9 +1,34 @@
 package myPackage;
+import java.util.regex.*;
+import java.util.Scanner;
 
 public class Regex {
+	boolean validName;
+	String firstName;
 
+	Scanner sc = new Scanner(System.in);
+
+	public void validateFirstName(){
+		System.out.println("Enter First Name (It should start with Capital Letter and have atleast 3 letters)");
+		firstName = sc.next();
+
+		validName = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
+		System.out.println(validName);
+		if(validName == true){
+			System.out.println("First Name is valid");
+		}
+		else{
+			System.out.println("First Name is not valid");
+			
+		}
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to a User registration problem using Reges");
+		
+		Regex user = new Regex();
+
+		user.validateFirstName();
 
 	}
 
